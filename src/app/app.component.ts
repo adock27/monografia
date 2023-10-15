7b8bed92b581b7235243874cc9b78d34aff4b951
adock27 <andersondumet@gmail.com>
2023-10-14 21:18:35 -0500

    up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { FormControl } from '@angular/forms';
-import { articles } from './json/articles';
+import { card } from './components/components';
 
 @Component({
   selector: 'app-root',
@@ -11,32 +11,18 @@ import { articles } from './json/articles';
 export class AppComponent {
   title = 'monografia';
 
-  articles: any;
+  card: any;
+  cardGeneratorField = new FormControl(0);
+  lines = new FormControl(0);
 
-
-
-  size = new FormControl(8);
-
-  constructor(private http: HttpClient) { }
+  constructor() { }
 
   ngOnInit() {
-   this.articles = articles
+    this.card = card
   }
 
 
-  state: any;
 
-
-  validateField(number: any) {
-
-    if (isNaN(number)) { this.state = 'No es un numero'; return false };
-    if (number > this.articles.length) { this.state = 'No puede ser mayor'; return false };
-    if (number == null) { this.state = 'No puede estar vacio'; return false };
-    if (number < 1) { this.state = 'No puede ser 0'; return false };
-    if (number > 1000) { this.state = 'Rendimiento lento'; return true };
-    this.state = ''
-    return true;
-  }
 
   // ng build --base-href "./" --deploy-url "./"
 
