e90893495207872de4582a6d3cc8f49ab2685ae1
adock27 <andersondumet@gmail.com>
2023-11-13 16:10:59 -0500

    another update

--- src/app/pages/boostrap/boostrap.component.ts
+++ src/app/pages/boostrap/boostrap.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { card } from 'src/app/components/components';
+
 @Component({
   templateUrl: './boostrap.component.html',
   styleUrls: ['./boostrap.component.css']
@@ -10,8 +11,20 @@ export class BoostrapComponent {
   cardGeneratorField = new FormControl(0);
   card: any;
   constructor() { }
-
+  counter: any;
+  msg:any;
   ngOnInit() {
     this.card = card
+    this.counter = 0
+    this.msg = 'Iniciar'
+  }
+
+  initTest() {
+    if (this.counter == 0) {
+      this.counter = 10000
+      this.msg = ''
+    } else {
+      this.counter = 0
+    }
   }
 }
--- src/app/pages/tailwind/tailwind.component.ts
+++ src/app/pages/tailwind/tailwind.component.ts
@@ -10,9 +10,22 @@ export class TailwindComponent {
 
   cardGeneratorField = new FormControl(0);
   card: any;
+  counter: any;
+  msg: any;
   constructor() { }
 
   ngOnInit() {
     this.card = card
+    this.counter = 0
+    this.msg = 'Iniciar'
+  }
+
+  initTest() {
+    if (this.counter == 0) {
+      this.counter = 10000
+      this.msg = ''
+    } else {
+      this.counter = 0
+    }
   }
 }
